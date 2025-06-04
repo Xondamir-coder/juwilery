71bfdc328e5cac658462bbbad0d71db33afb5f4c
Xondamir Nazrullayev <xondamirnazrullayev@mail.ru>
2025-06-04 19:00:16 +0500

    header done

---
+++ src/js/main.js
@@ -0,0 +1,17 @@
+const handleToggleDropdowns = () => {
+	const togglers = document.querySelectorAll('[data-toggle]');
+	togglers.forEach(t => {
+		t.addEventListener('click', () => {
+			document.getElementById(t.dataset.toggle).classList.toggle('active');
+		});
+	});
+	document.addEventListener('click', e => {
+		if (!e.target.closest('[data-toggle]')) {
+			togglers.forEach(t => {
+				document.getElementById(t.dataset.toggle).classList.remove('active');
+			});
+		}
+	});
+};
+
+handleToggleDropdowns();
--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,13 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, 'src')
-		}
-	},
 	build: {
 		rollupOptions: {
 			input: {
-				main: 'index.html'
+				main: 'index.html',
+				about: 'about.html',
+				contacts: 'contacts.html',
+				faq: 'faq.html'
 			}
 		}
 	}
